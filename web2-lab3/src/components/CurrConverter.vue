d31b7f5b28543b453ff22e2e76d81a2abe20d9fa
Lucija <lucija.strejcek@fer.hr>
2022-01-03 12:45:48 +0100

    Add scoped style

--- web2-lab3/src/components/CurrConverter.vue
+++ web2-lab3/src/components/CurrConverter.vue
@@ -1,11 +1,11 @@
 <template>
   <small-card>
     <h3>Currency converter</h3>
-    <form>
+    <form class="card">
       <div class="form-group row">
         <label for="hrvatskakuna" class="col-sm-4 col-form-label"
-          >Hrvatska kuna</label
-        >
+          >Hrvatska kuna (kn)
+        </label>
         <div class="col-8">
           <input
             type="number"
@@ -17,7 +17,7 @@
         </div>
       </div>
       <div class="form-group row">
-        <label for="euro" class="col-4 col-form-label">Euro</label>
+        <label for="euro" class="col-4 col-form-label">Euro (€)</label>
         <div class="col-8">
           <input
             type="number"
@@ -50,3 +50,18 @@ export default {
   },
 };
 </script>
+
+<style scoped>
+.card {
+  border: green 10px solid;
+  border-radius: 5px;
+  padding: 20px 0px 20px 0px;
+}
+input[type="number"] {
+  padding: 10px 12px;
+  margin: 8px 0;
+}
+label {
+  color: green;
+}
+</style>
